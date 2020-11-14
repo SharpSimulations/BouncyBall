a5cdda8552a6371042e69c9bb22619d6a049c439
georgiosmichalis <george@recursiveengineering.com>
2020-11-14 23:49:58 +0200

    added ball as a class

---
+++ BouncyBall/Ball.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing.Drawing2D;
+using System.Drawing;
+
+namespace BouncyBall
+{
+    class Ball
+    {
+        private int m_BallWidth;
+        private int m_BallHeight;
+        private int m_BallposX, m_BallposY;   // Position.
+        private int m_BallVelX, m_BallVelY; // Velocity.
+
+        public Ball(int ballDiameter, int ballVelX, int ballVelY, int ballPosX, int ballPosY)
+        {
+
+            m_BallWidth = m_BallHeight = ballDiameter / 2;
+            m_BallposX = ballPosX;
+            m_BallposY = ballPosY;
+            m_BallVelX = ballVelX;
+            m_BallVelY = ballVelY;
+        }
+
+        public void DrawBall(Graphics graphics)
+        {
+            graphics.FillEllipse(Brushes.Red, m_BallposX, m_BallposY, m_BallWidth, m_BallHeight);
+            graphics.DrawEllipse(Pens.Black, m_BallposX, m_BallposY, m_BallWidth, m_BallHeight);
+        }
+
+
+        public void UpdatePosition(int boundingBoxWidth, int boundingBoxHeight)
+        {
+            m_BallposX += m_BallVelX; //update the position
+            if (m_BallposX < 0)
+            {
+                m_BallVelX = -m_BallVelX;
+            }
+            else if (m_BallposX + m_BallWidth > boundingBoxWidth)
+            {
+                m_BallVelX = -m_BallVelX;
+            }
+
+            m_BallposY += m_BallVelY;
+            if (m_BallposY < 0)
+            {
+                m_BallVelY = -m_BallVelY;
+            }
+            else if (m_BallposY + m_BallHeight > boundingBoxHeight)
+            {
+                m_BallVelY = -m_BallVelY;
+            }
+        }
+    }
+}
--- BouncyBall/Form1.cs
+++ BouncyBall/Form1.cs
@@ -14,37 +14,45 @@ namespace BouncyBall
     public partial class Form1 : Form
     {
         // Some drawing parameters.
-        private const int m_BallWidth = 50;
-        private const int m_BallHeight = 50;
-        private int m_BallposX, m_BallposY;   // Position.
-        private int m_BallVelX, m_BallVelY; // Velocity.
-        private int m_InitVelocity = 8;
+        List<Ball> balls = new List<Ball>();
+        private int m_MaxBallSize = 100;
+        private int m_MaxVelocity = 30;
         private const int m_Frequency = 60;
         private const decimal m_UpdateTime = 1000 / m_Frequency;
+        private int m_MaxNumberOfBalls = 4;
 
+        
         public Form1()
         {
             InitializeComponent();
         }
+
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
             //for smooth update
             e.Graphics.SmoothingMode = SmoothingMode.AntiAlias;
             e.Graphics.Clear(BackColor);
-            e.Graphics.FillEllipse(Brushes.Red, m_BallposX, m_BallposY, m_BallWidth, m_BallHeight);
-            e.Graphics.DrawEllipse(Pens.Black, m_BallposX, m_BallposY, m_BallWidth, m_BallHeight);
+            foreach (Ball ball in balls)
+            { 
+                ball.DrawBall(e.Graphics);
+            }
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // On load Pick a random start position and velocity.
             Random rnd = new Random();
-            m_BallVelX = rnd.Next(1, m_InitVelocity);
-            m_BallVelY = rnd.Next(1, m_InitVelocity);
-            m_BallposX = rnd.Next(0, ClientSize.Width - m_BallWidth);
-            m_BallposY = rnd.Next(0, ClientSize.Height - m_BallHeight);
-
+            // On load Pick a random start position and velocity.
+            for (int i = 0; i < m_MaxNumberOfBalls; i++)
+            { 
+                int ballSize = rnd.Next(1, m_MaxBallSize);
+            
+                balls.Add(new Ball(ballSize, rnd.Next(1, rnd.Next(1, m_MaxVelocity)), 
+                                            rnd.Next(1, rnd.Next(1, m_MaxVelocity)), 
+                                            rnd.Next(0, ClientSize.Width - ballSize), 
+                                            rnd.Next(0, ClientSize.Height - ballSize)));
+            }
+            
             // Use double buffering to reduce flicker.
             this.SetStyle(
                 ControlStyles.AllPaintingInWmPaint |
@@ -63,26 +71,10 @@ namespace BouncyBall
          // Update the ball's position, bouncing if necessary.
         private void MyTimer_Tick(object sender, EventArgs e)
         {
-            m_BallposX += m_BallVelX; //update the position
-            if (m_BallposX < 0)
+            foreach (Ball ball in balls)
             {
-                m_BallVelX = -m_BallVelX;
+                ball.UpdatePosition(ClientSize.Width, ClientSize.Height);
             }
-            else if (m_BallposX + m_BallWidth > ClientSize.Width)
-            {
-                m_BallVelX = -m_BallVelX;
-            }
-
-            m_BallposY += m_BallVelY;
-            if (m_BallposY < 0)
-            {
-                m_BallVelY = -m_BallVelY;
-            }
-            else if (m_BallposY + m_BallHeight > ClientSize.Height)
-            {
-                m_BallVelY = -m_BallVelY;
-            }
-
             Refresh();
         }
     }
